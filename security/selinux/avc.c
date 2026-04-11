5a27e692ed010ab7c08b11a96f44ce560d29b227
Lup Gabriel <gwolf2u.website@gmail.com>
2026-04-11 13:44:39 +0300

    susfs: fix misplaced avc_dump_query spoofing block in selinux/avc.c
    The susfs audit-log spoofing code for avc_dump_query was accidentally placed
    inside avc_init() during the susfs v2.0.0 patch application.  This caused
    build errors because the variables tsid, rc, and ab (which are local to
    avc_dump_query) are not in scope inside avc_init():
    
      error: use of undeclared identifier 'tsid'
      error: use of undeclared identifier 'rc'
      error: use of undeclared identifier 'ab'
      error: unused label 'bypass_orig_flow'
    
    Move the block to its correct location inside avc_dump_query(), immediately
    after the ssid context lookup and before the tsid context lookup, so the
    goto properly skips the normal tsid-to-context translation when spoofing
    is active.

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -163,6 +163,16 @@ static void avc_dump_query(struct audit_buffer *ab, u32 ssid, u32 tsid, u16 tcla
 		kfree(scontext);
 	}
 
+#ifdef CONFIG_KSU_SUSFS
+	if (unlikely(tsid == susfs_ksu_sid && susfs_is_avc_log_spoofing_enabled)) {
+		if (rc)
+			audit_log_format(ab, " tsid=%d", susfs_priv_app_sid);
+		else
+			audit_log_format(ab, " tcontext=%s", "u:r:priv_app:s0:c512,c768");
+		goto bypass_orig_flow;
+	}
+#endif
+
 	rc = security_sid_to_context(tsid, &scontext, &scontext_len);
 	if (rc)
 		audit_log_format(ab, " tsid=%d", tsid);
@@ -190,16 +200,6 @@ void __init avc_init(void)
 	for (i = 0; i < AVC_CACHE_SLOTS; i++) {
 		INIT_HLIST_HEAD(&avc_cache.slots[i]);
 		spin_lock_init(&avc_cache.slots_lock[i]);
-#ifdef CONFIG_KSU_SUSFS
-	if (unlikely(tsid == susfs_ksu_sid && susfs_is_avc_log_spoofing_enabled)) {
-		if (rc)
-			audit_log_format(ab, " tsid=%d", susfs_priv_app_sid);
-		else
-			audit_log_format(ab, " tcontext=%s", "u:r:priv_app:s0:c512,c768");
-		goto bypass_orig_flow;
-	}
-#endif
-
 	}
 	atomic_set(&avc_cache.active_nodes, 0);
 	atomic_set(&avc_cache.lru_hint, 0);
